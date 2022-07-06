bba5c76bfc301f437df2a88aa3cb856420d73e12
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2022-07-06 17:05:47 +0100

    backup

--- 04_Recurrent-Neural-Networks/05_Embeddings-Word2Vec/data.sh
+++ 04_Recurrent-Neural-Networks/05_Embeddings-Word2Vec/data.sh
@@ -3,4 +3,4 @@
 mkdir -p data
 
 wget https://s3.amazonaws.com/video.udacity-data.com/topher/2018/October/5bbe6499_text8/text8.zip -O data/text8.zip
-unzip data/text8.zip data/text8
+unzip data/text8.zip -d data
